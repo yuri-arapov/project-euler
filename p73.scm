5f31719c025b445388f1ca1082251768c5638948
Yuri <yuridichesky@gmail.com>
2009-05-15 14:25:17 +0400

    problem 73 solved

--- p73.scm
+++ p73.scm
@@ -19,8 +19,54 @@
 ;; How many fractions lie between 1/3 and 1/2 in the sorted set of reduced
 ;; proper fractions for d ≤ 10,000?
 ;;
-;; Answer: ???
-;;      
+;; Answer: 5066251
+;;
+
+
+;;
+;; Accumulator.
+;;
+(define
+  (accum      ;; Accumulate the result of iterating
+    from      ;;   from 'from' (inclusive)
+    to        ;;   to 'to' (inclusive)
+    accum-fn  ;;   with accumulating function 'accum-fn'
+    trans-fn  ;;   and transforming each iterator with 'trans-fn' function,
+    init)     ;;   and initial accumulation be equal to 'init'.
+  (let loop ((n from)
+             (r init))
+    (if (> n to)
+      r
+      (loop (+ n 1) (accum-fn r (trans-fn n))))))
+
+
+(define (lower-bound d n)
+  (let ((x (quotient d (/ 1 n))))
+    (if (> (/ x d) n) x (+ x 1))))
+
+
+(define (upper-bound d n)
+  (let ((x (quotient d (/ 1 n))))
+    (if (< (/ x d) n) x (- x 1))))
+
+
+(define (f maxd)
+  (accum 1
+         maxd
+         +
+         (lambda (d)
+           (accum
+             (lower-bound d 1/3)
+             (upper-bound d 1/2)
+             +
+             (lambda (n) (if (= 1 (gcd n d)) 1 0))
+             0))
+         0))
+
+
+(define (p73)
+  (f 10000))
+
 
 ;; end of file
 ;; vim: ts=4 sw=4

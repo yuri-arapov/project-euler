82f9d407d04bfbfa53e7c02da05341bf7690e039
Yuri <yuridichesky@gmail.com>
2009-12-29 20:03:29 +0300

    iterative version of (number->digits num)

--- number-digits.scm
+++ number-digits.scm
@@ -8,18 +8,18 @@
 
 
 (define (number->digits n)
-;; return list of digits this number is made of
+;; return list of decimal digits this number is made of
 ;; (right to left: lower digits go first)
 ;;
-;;  (define (iter ls n)
-;;    (if (< n 10)
-;;      (cons n ls)
-;;      (iter (cons (remainder n 10) ls) (quotient n 10))))
-;;  (iter '() n))
-  (if (< n 10)
-    (list n)
-    (cons (remainder n 10) 
-          (number->digits (quotient n 10)))))
+  (define (iter ls n)
+    (if (< n 10)
+      (cons n ls)
+      (iter (cons (remainder n 10) ls) (quotient n 10))))
+  (iter '() n))
+;;  (if (< n 10)
+;;    (list n)
+;;    (cons (remainder n 10) 
+;;          (number->digits (quotient n 10)))))
 
 ;;(define (number->digits n)
 ;;  (if (< n 10)

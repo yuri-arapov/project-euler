b921e0441a91b0fda2b806b3a51de1081418d969
yar <yar@yar.yarhome>
2010-02-22 17:31:41 +0300

    fast primality test added

--- primes-1000000.scm
+++ primes-1000000.scm
@@ -3,7 +3,7 @@
 ;; Primes under 1000000.
 
 
-;; Load primes from file
+;; Load primes from file.
 ;;
 (define (primes-1000000-load)
 
@@ -20,6 +20,21 @@
 ;; List of primes under 1000000.
 ;;
 (define primes-1000000 (primes-1000000-load))
+
+
+;; Bitvector for fast primality test.
+;;
+(define primes-1000000-bv (make-bitvector 1000001 #f))
+(for-each (lambda (n) (bitvector-set! primes-1000000-bv n #t)) primes-1000000)
+
+
+;; Primality test.
+;;
+(define (primes-1000000-prime? n)
+  (cond ((or (< n 2) (> n 1000000))
+         #f)
+        (else
+          (bitvector-ref primes-1000000-bv n))))
 
 
 ;; end of file

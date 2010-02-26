12be7668d7427e0785ed7e3b5d0e2965fa7110ee
yar <yar@yar.yarhome>
2010-02-27 00:10:21 +0300

    minor refactoring

--- uniq.scm
+++ uniq.scm
@@ -3,14 +3,17 @@
 
 ;; Remove equal consecutive elements from the list.
 ;;
-(define (uniq s)
+(define (uniqp s p)
   (if (null? s)
     s
     (let loop ((s   (cdr s))
                (res (list (car s))))
       (cond ((null? s)                  (reverse res))
-            ((= (car s) (car res))      (loop (cdr s) res))
+            ((p (car s) (car res))      (loop (cdr s) res))
             (else                       (loop (cdr s) (cons (car s) res)))))))
+
+
+(define (uniq s) (uniqp s =))
 
 
 ;; Alias for uniq

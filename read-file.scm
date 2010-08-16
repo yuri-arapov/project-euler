59a0647eb47efa9f6f56cdf497467d2f07793336
yuridichesky <yuridichesky@gmail.com>
2010-08-16 15:54:21 +0400

    (read-file-with ...) function added

--- read-file.scm
+++ read-file.scm
@@ -2,14 +2,20 @@
 ;;
 
 
-(define (read-file file)
+(define (read-file-with file proc)
   (define (read-lines port)
     (let loop ((line (read-line port))
                (res '()))
       (if (eof-object? line)
         (reverse res)
-        (loop (read-line port) (cons line res)))))
+        (loop (read-line port) (cons (proc line) res)))))
   (call-with-input-file file read-lines))
+
+
+
+(define (read-file file)
+  (read-file-with file (lambda (line) line)))
+
 
 
 ;; end of file

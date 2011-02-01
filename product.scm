2eb87765e0c3b52cbe9e1a62e54c24ddbf8894f1
yuridichesky <yuridichesky@gmail.com>
2011-02-01 23:32:42 +0300

    loop replaced with fold

--- product.scm
+++ product.scm
@@ -2,16 +2,24 @@
 ;; Apply operation op for each pair of elements from s1 and s2,
 ;; return list of resultant values
 ;;
+
+;;(define (each-to-each op s1 s2)
+;;  (let loop ((i1 s1)
+;;             (i2 s2)
+;;             (res '()))
+;;    (cond ((null? i1)
+;;           (reverse res))
+;;          ((null? i2)
+;;           (loop (cdr i1) s2 res))
+;;          (else
+;;            (loop i1 (cdr i2) (cons (op (car i1) (car i2)) res))))))
+
 (define (each-to-each op s1 s2)
-  (let loop ((i1 s1)
-             (i2 s2)
-             (res '()))
-    (cond ((null? i1)
-           (reverse res))
-          ((null? i2)
-           (loop (cdr i1) s2 res))
-          (else
-            (loop i1 (cdr i2) (cons (op (car i1) (car i2)) res))))))
+  (reverse
+    (fold (lambda (i1 res1)
+            (fold (lambda (i2 res2) (cons (op i1 i2) res2)) res1 s2))
+          '()
+          s1)))
 
 
 ;; TODO: for-each-to-each

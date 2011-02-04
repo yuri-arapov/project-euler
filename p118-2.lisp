4212ce4a219cc4e43f1d78b11c9f2d0eb798409e
yuridichesky <yuridichesky@gmail.com>
2011-02-04 12:21:28 +0300

    cosmetics

--- p118-2.lisp
+++ p118-2.lisp
@@ -15,45 +15,12 @@
 ;; Answer: 44680
 
 
-(load "product.lisp")
-(load "permutations.lisp")
-(load "miller-rabin-primality-test.lisp")
+(load "product")
+(load "permutations")
+(load "miller-rabin-primality-test")
 
 
-;; Currying functions.
-;; Found here:
-;;   http://cl-cookbook.sourceforge.net/functions.html#curry
-(declaim (ftype (function (function &rest t) function) curry)
-         (inline curry))
-
-(defun curry (function &rest args)
-  (lambda (&rest more-args)
-    (apply function (append args more-args))))
-
-
-;; Scheme-to-List helper
-(defun filter (proc list)
-  (remove-if-not proc list))
-
-
-;; Scheme-to-List helper
-(defun for-each (proc list)
-  (dolist (x list) (funcall proc x)))
-
-
-;; Scheme-to-List helper
-(defun fold (proc init list)
-  (reduce #'(lambda (res i) (funcall proc i res))
-          list
-          :initial-value init))
-
-
-;; Scheme-to-List helper
-(defun fold-right (proc init list)
-  (reduce #'(lambda (i res) (funcall proc i res))
-          list
-          :from-end t
-          :initial-value init))
+(load "lisp-utils")
 
 
 (defun println (s) (for-each (curry #'format t "~a~%") s))

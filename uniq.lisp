71ebe70c39d68c76bf8c207241d2aa432ef2b121
yuridichesky <yuridichesky@gmail.com>
2011-02-17 23:31:43 +0300

    (uniq ...) for Lisp

---
+++ uniq.lisp
@@ -0,0 +1,19 @@
+;; uniq.lisp
+
+
+;; Remove equal consecutive elements from the list.
+;;
+(defun uniq (s &key (equal-proc #'=))
+  (if (null s)
+    s
+    (nreverse 
+      (reduce 
+        #'(lambda (res e) 
+            (if (funcall equal-proc (car res) e) 
+              res 
+              (cons e res)))
+        s
+        :initial-value (list (car s))))))
+
+
+;; end of file

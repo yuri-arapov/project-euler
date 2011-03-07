0d1924ac7a69af5104306fd878b2cbdd7d8d1c6b
yuridichesky <yuridichesky@gmail.com>
2011-03-07 21:22:13 +0300

    refactoring

--- p132.scm
+++ p132.scm
@@ -24,13 +24,6 @@
 (load "miller-rabin-primality-test.scm")
 
 
-;; Get prime that follows by given prime.
-(define (next-prime p)
-  (let ((n (+ 2 p)))
-    (if (prime? n) n
-      (next-prime n))))
-
-
 ;; Solve problem 132.
 (define (p132)
   (apply 
@@ -38,7 +31,7 @@
     (construct-list-length
       40 ; list of 40 elements
 
-      (lambda (p) (and (= 1 (mod-pow 10 1000000000 p)) p))
+      (lambda (p) (and (prime? p) (= 1 (mod-pow 10 1000000000 p)) p))
       ; R(n) = (10^n - 1)/9 by definition.
       ;
       ; So 9*R(n) + 1 = 10^n
@@ -53,7 +46,7 @@
       7 ; first prime to test: the 2 is prime but it's not a factor of 11111...111,
         ; the 3 is prime too but 10^9 of 1s can not be divided by 3.
 
-      next-prime)))
+      (curry + 2))))
 
 
 ;; end of file

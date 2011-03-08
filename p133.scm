004b1b1a1b2ec3aa1371536291dd7289b39cbb79
yuridichesky <yuridichesky@gmail.com>
2011-03-08 19:32:40 +0300

    problem 133 solved

---
+++ p133.scm
@@ -0,0 +1,53 @@
+;; 2011-03-08
+;;
+;; Yuri Arapov <yuridichesky@gmail.com>
+;;
+;; http://projecteuler.net/index.php?section=problems&id=133
+;;
+;; Problem 133
+;; 01 December 2006
+;;
+;; A number consisting entirely of ones is called a repunit. We shall define
+;; R(k) to be a repunit of length k; for example, R(6) = 111111.
+;;
+;; Let us consider repunits of the form R(10^n).
+;;
+;; Although R(10), R(100), or R(1000) are not divisible by 17, R(10000) is
+;; divisible by 17. Yet there is no value of n for which R(10^n) will divide by
+;; 19. In fact, it is remarkable that 11, 17, 41, and 73 are the only four
+;; primes below one-hundred that can be a factor of R(10^n).
+;;
+;; Find the sum of all the primes below one-hundred thousand that will never be
+;; a factor of R(10^n).
+;;
+;; Answer: 453647705
+
+
+(load "read-file.scm")
+(load "scheme-utils.scm")   ; mod-pow
+
+
+(define ten^30 (expt 10 30))
+
+
+(define (factor? p)
+  (= 1 (mod-pow 10 ten^30 p)))
+
+
+(define (p133-ex limit)
+  (apply +
+    (filter 
+      (compose not factor?)
+      (unfold
+        (lambda (s) (> (car s) limit))  ; stop when primes greater then given limit
+        (lambda (s) (car s))            ; prime
+        (lambda (s) (cdr s))            ; next prime
+        (read-file-with "primes-1000000" string->number)))))
+
+
+(define (p133)
+  (p133-ex 100000))
+
+
+;; end of file
+;; vim: sw=4 ts=4

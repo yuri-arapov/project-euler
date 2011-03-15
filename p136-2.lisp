8157fc03e35667130ff729458a1f7ff02a68d959
yuridichesky <yuridichesky@gmail.com>
2011-03-15 12:00:25 +0300

    refactoring

--- p136-2.lisp
+++ p136-2.lisp
@@ -8,10 +8,10 @@
 ;; 29 December 2006
 ;;
 ;; The positive integers, x, y, and z, are consecutive terms of an arithmetic
-;; progression. Given that n is a positive integer, the equation, x2  y2  z2 =
-;; n, has exactly one solution when n = 20:
+;; progression. Given that n is a positive integer, the equation, 
+;; x^2 - y^2 - z^2 = n, has exactly one solution when n = 20:
 ;;
-;; 132  102  72 = 20
+;; 13^2 - 10^2 - 7^2 = 20
 ;;
 ;; In fact there are twenty-five values of n below one hundred for which the
 ;; equation has a unique solution.
@@ -21,39 +21,20 @@
 ;; Answer: 2544559
 
 
-(defun make-bitvector (n)       (make-array n :element-type '(unsigned-byte 1)))
-(defun bitvector-ref  (bv i)    (not (zerop (aref bv i))))
-(defun bitvector-set! (bv i val)(setf (aref bv i) (if val 1 0)))
-
-(defun quotient (n d) (truncate (/ n d)))
-
-
 (defun p136 (&optional (limit 50000000))
 
-  (let ((hits (make-bitvector limit))
-        (full (make-bitvector limit)))
+  (let ((hits (make-array limit :element-type '(unsigned-byte 2))))
 
-    (defun hit?  (n) (bitvector-ref hits n))
-    (defun full? (n) (bitvector-ref full n))
-
-    (defun hit! (n)
-      (if (hit? n)
-        (bitvector-set! full n t)
-        (bitvector-set! hits n t)))
+    (defun hit! (n) (if (< (aref hits n) 2) (incf (aref hits n))))
 
     (loop for y from 2 to (1- limit) do
 
-      (progn
-
-        (if (zerop (rem y 10000))
-          (format t "~a~%" y))
-
-        (loop for k from (1+ (quotient y 4))
-                    to (min (1- y) (quotient (+ (1- limit) (* y y)) (* 4 y)))
-           do (hit! (- (* 4 k y) (* y y))))))
+        (loop for k from (1+ (truncate (/ y 4)))
+                    to (min (1- y) (truncate (/ (+ (1- limit) (* y y)) (* 4 y))))
+           do (hit! (- (* 4 k y) (* y y)))))
 
     (loop for n from 0 to (1- limit)
-          counting (and (not (full? n)) (hit? n)) into res
+          counting (= 1 (aref hits n)) into res
           finally (return res))))
 
 
--- p136-2.scm
+++ p136-2.scm
@@ -8,10 +8,10 @@
 ;; 29 December 2006
 ;;
 ;; The positive integers, x, y, and z, are consecutive terms of an arithmetic
-;; progression. Given that n is a positive integer, the equation, x2  y2  z2 =
-;; n, has exactly one solution when n = 20:
+;; progression. Given that n is a positive integer, the equation, 
+;; x^2 - y^2 - z^2 =  n, has exactly one solution when n = 20:
 ;;
-;; 132  102  72 = 20
+;; 13^2 - 10^2 - 7^2 = 20
 ;;
 ;; In fact there are twenty-five values of n below one hundred for which the
 ;; equation has a unique solution.

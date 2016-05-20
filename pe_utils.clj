09f544e27e40b532841c5e23aa4cd880642c8a09
Yuri Arapov <yarapov@loaddynamix.com>
2016-05-20 20:44:02 +0300

    number->digits refactored to accept optional base

--- pe_utils.clj
+++ pe_utils.clj
@@ -1,10 +1,13 @@
 ;; Project Euler utils.
 
 
-(defn number->digits [n]
-  (if (zero? n) '(0)
-      (loop [res '(), n n]
-        (if (zero? n) res
-            (recur (conj res (int (rem n 10))) (quot n 10))))))
+(defn number->digits
+  ([n]
+   (number->digits n 10))
+  ([n base]
+   (if (zero? n) '(0)
+       (loop [res '(), n n]
+         (if (zero? n) res
+             (recur (conj res (int (rem n base))) (quot n base)))))))
 
 ;; end of file

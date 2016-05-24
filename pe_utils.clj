d38b4facac715909dc32174779b474264b06fc88
Yuri Arapov <yarapov@loaddynamix.com>
2016-05-24 19:37:41 +0300

    digits->number added

--- pe_utils.clj
+++ pe_utils.clj
@@ -10,4 +10,11 @@
          (if (zero? n) res
              (recur (conj res (int (rem n base))) (quot n base)))))))
 
+
+(defn digits->number
+  ([digits]
+   (digits->number digits 10))
+  ([digits base]
+   (reduce (fn [res d] (+ (* res base) d)) 0 digits)))
+
 ;; end of file

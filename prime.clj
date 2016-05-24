abb1eb9fc27bca43bafb84b1166014ec073aa482
yuridichesky <yuridichesky@gmail.com>
2016-05-24 11:29:51 +0400

    prime? fixed (1 is not prime number)

--- prime.clj
+++ prime.clj
@@ -2,7 +2,8 @@
 
 
 (defn prime? [n]
-  (cond (= 2 n) true
+  (cond (= 1 n) false
+        (= 2 n) true
         (zero? (rem n 2)) false
         :else (loop [d 3]
                 (cond (> (* d d) n) true

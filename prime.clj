825f14598a151efe7913fde644eaed2c8b992af8
yuridichesky <yuridichesky@gmail.com>
2016-05-24 11:22:55 +0400

    prime? fixed (2 is the prime number)

--- prime.clj
+++ prime.clj
@@ -2,11 +2,12 @@
 
 
 (defn prime? [n]
-  (if (zero? (rem n 2)) false
-    (loop [d 3]
-      (cond (> (* d d) n) true
-            (zero? (rem n d)) false
-            :else (recur (+ 2 d))))))
+  (cond (= 2 n) true
+        (zero? (rem n 2)) false
+        :else (loop [d 3]
+                (cond (> (* d d) n) true
+                      (zero? (rem n d)) false
+                      :else (recur (+ 2 d))))))
 
 
 ;; end of file

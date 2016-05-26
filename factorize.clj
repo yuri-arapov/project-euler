0d4985b197deb424136ba1a1af9dfe33ab696bbf
yuridichesky <yuridichesky@gmail.com>
2016-05-26 22:38:38 +0400

    factorization improvements

--- factorize.clj
+++ factorize.clj
@@ -11,12 +11,16 @@
 (defn factorize
   "return list of prime factors given number consists of"
   [n]
-  (if (zero? n) '()
-    (loop [res '(), [p & _ :as primes] (factor-png :primes), n n]
-      (if (= 1 n) (reverse res)
-        (cond (> (* p p) n)     (recur (cons n res) primes 1)
-              (zero? (rem n p)) (recur (cons p res) primes (/ n p))
-              :else             (recur res (rest primes) n))))))
+  (loop [res '(), [p & _ :as primes] (factor-png :primes), n n]
+    (cond (> (* p p) n)
+            (cond (not (pos? n)) '()
+                  (empty? res) (list n)
+                  (>= n (first res)) (reverse (cons n res))
+                  :else (reverse res))
+          (zero? (rem n p))
+            (recur (cons p res) primes (/ n p))
+          :else
+            (recur res (next primes) n))))
 
 
 ;; end of file
